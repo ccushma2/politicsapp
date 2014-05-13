0e0c1b6da46b7e5f3fcde1b037ca9a769e563c5b
ccushma2 <ccushma2@bates.edu>
2014-05-12 21:07:30 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Politicsapp::Application.config.secret_key_base = '546af5bcf0ba7ba03eda947884cd6b1faecba4260eec3b5dba1c27510b28dbbcef741b2a82c4bdda4d4c8320ddbffd837a395d42d134f68b0e2cb567e711f098'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Politicsapp::Application.config.session_store :cookie_store, key: '_politicsapp_session'
