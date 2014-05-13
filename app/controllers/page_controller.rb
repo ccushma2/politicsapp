405c8f542ccbf82a85373aac80710f415a652add
ccushma2 <ccushma2@bates.edu>
2014-05-13 18:41:50 -0400

    Jumbotron, button, and /zip

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,7 @@
 class PageController < ApplicationController
   def index
   end
+
+  def zip
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Politicsapp::Application.routes.draw do
   root "page#index"
+
+  get '/zip'=>"page#zip"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
